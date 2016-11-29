bcc083826f0eed48f392bb11612edd22e80e0e68
eiji mishiro <eiji346g@gmail.com>
2016-11-29 14:01:34 +0900

    vn use parent class save_article

--- app/models/matcha/vn.rb
+++ app/models/matcha/vn.rb
@@ -2,17 +2,4 @@ class Matcha::Vn < Matcha::Crawler
   def initialize
     super
   end
-
-  #TODO vetnamizeする
-  def save_article num, paragraphs
-    paragraphs.each.with_index(1) do |row, index|
-      article = Article.new ({
-        issue_num: num,
-        country: country,
-        row_num: index,
-        text: row.text
-      })
-      article.save
-    end
-  end
 end
